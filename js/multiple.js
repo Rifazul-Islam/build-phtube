cf89bb5148e0d360c734a80cc330c5922f6b46d9
Rifazul <rifazul50@gmail.com>
2023-08-31 21:29:25 +0600

    insert the card and display show

--- js/multiple.js
+++ js/multiple.js
@@ -2,7 +2,7 @@
 const loadCategoryName = async()=>{
  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
  const data = await res.json()
- const categories = data.data
+ const categories = data?.data
  displayShowName(categories)
 }
 
@@ -12,15 +12,45 @@ const displayShowName = (categories) =>{
   
   categories.forEach(category =>{
   const tabDiv = document.createElement('div')
-   tabDiv.classList = ` ml-4`
+   tabDiv.classList = `ml-4`
   tabDiv.innerHTML = `
-   <a class="tab btn-ghost normal-case bg-slate-200 rounded"> ${category.category} </a> 
+   <a onclick="handlerIdLoad('${category?.category_id}')" class="tab btn-ghost normal-case bg-slate-200 rounded"> ${category?.category} </a> 
   `
   tabContainer.appendChild(tabDiv)
 
   })
 
 }
+
+// handler categoryId 
+const handlerIdLoad= async(categoryId) =>{
+   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+   const data = await res.json()
+   const categories = data.data;
+   handlerCategoryCard(categories)
+}
+
+const handlerCategoryCard = (categories) =>{
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = '';
+   categories.forEach(category =>{
+   const cardDiv = document.createElement('div')
+   cardDiv.classList = `card card-compact bg-base-300 shadow-xl p-2`
+   cardDiv.innerHTML = `
+         <figure><img class="h-40 w-full" src="${category?.thumbnail}" alt="Shoes" /></figure>
+         <div class="card-body">
+         <h2 class="card-title">Shoes!</h2>
+         <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+       <button class="btn btn-primary">Buy Now</button>
+     </div>
+   </div>   
+   
+   `
+    cardContainer.appendChild(cardDiv)
+   })
+}
+
 
 
 loadCategoryName()
