1b342572cccc6383eeb4c07716fe4b4af5dedf81
Rifazul <rifazul50@gmail.com>
2023-09-01 10:05:08 +0600

    add convert to hours and minute

--- js/multiple.js
+++ js/multiple.js
@@ -28,10 +28,24 @@ const handlerIdLoad= async(categoryId="1000") =>{
    const data = await res.json()
    const categories = data.data;
    handlerCategoryCard(categories)
+   handlerSort(categories)
 }
 
 //default id call 
 handlerIdLoad()
+
+
+// Sort system 
+const handlerSort = (categories) =>{
+ 
+
+   // sortingCategory = categories.sort(function(a, b){return b.others?.views.slice(0,3) - a.others?.views.slice(0,3)});
+//   if(!true){
+//      console.log('hello');
+//   }else{
+//     console.log('none');
+//   }
+}
 
 
 const handlerCategoryCard = (categories) =>{
@@ -50,15 +64,43 @@ const handlerCategoryCard = (categories) =>{
 
 
    categories.forEach(category =>{
+      
+   const number = category?.others?.posted_date || ' ';
+   const numberConvert = parseInt(number);
+   
+// convert Hours and Minute
+   const  convertHoursMinute = (numberConvert) =>{
+      if(!isNaN(numberConvert)){
+         if(numberConvert % 60 !== 0){
+            const hour = numberConvert / 60 ;
+            const houred = Math.floor(hour);
+            const hours = houred / 60 ;
+            const currentHours = Math.floor(hours)
+            
+            const minute = numberConvert % 60 ;
+           return ` ${currentHours} Hrs   ${minute} minute ago `
+         }
+      }
+    
+   }
+   
+  const result =  convertHoursMinute(numberConvert)
+
+
+
+
+
    const cardDiv = document.createElement('div')
    cardDiv.classList = `card card-compact bg-base-300 shadow-xl p-2`
    cardDiv.innerHTML = `
-         <figure><img class="h-40 w-full" src="${category?.thumbnail}" alt="Shoes" /></figure>
+          
+         <figure class="h-40 relative"><img class="w-full"  src="${category?.thumbnail}" alt="Shoes" />
+         
+         <p class="absolute right-3 bottom-3 text-white bg-black rounded p-1"> ${result ? result : ' '} </p>
+         </figure>
+         
          <div class="card-body">
           <div class="flex items-center">
-
-     
-
         <div class="avatar">
            <div class="w-12 rounded-full  ring-offset-base-100 ring-offset-2">
            <img src="${category?.authors[0]?.profile_picture}" />
@@ -86,3 +128,19 @@ const handlerCategoryCard = (categories) =>{
 
 
 loadCategoryName()
+
+
+
+
+
+ const handlerSor = () =>{
+   if(!clicked){
+      //some code here
+          console.log("working");
+      }else{
+         'non work'
+      }
+
+ }
+
+
